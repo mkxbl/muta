5c480f346b94d04dbc2a8aadb62f2ba8be86419b
zhounan <2627159529@qq.com>
2020-06-12 18:55:19 +0800

    fix asset service

---
+++ built-in-services/asset/src/lib.rs
@@ -0,0 +1,357 @@
+#[cfg(test)]
+mod tests;
+pub mod types;
+
+use std::collections::BTreeMap;
+
+use bytes::Bytes;
+
+use binding_macro::{cycles, genesis, service};
+use protocol::emit_event;
+use protocol::traits::{ExecutorParams, MetaGenerator, ServiceResponse, ServiceSDK, StoreMap};
+use protocol::types::{Address, DataMeta, Hash, MethodMeta, ServiceContext, ServiceMeta};
+
+use crate::types::{
+    ApproveEvent, ApprovePayload, Asset, AssetBalance, CreateAssetPayload, Event,
+    GetAllowancePayload, GetAllowanceResponse, GetAssetPayload, GetBalancePayload,
+    GetBalanceResponse, InitGenesisPayload, TransferEvent, TransferFromEvent, TransferFromPayload,
+    TransferPayload,
+};
+
+pub struct AssetService<SDK> {
+    sdk:    SDK,
+    assets: Box<dyn StoreMap<Hash, Asset>>,
+}
+
+#[service(Event)]
+impl<SDK: ServiceSDK> AssetService<SDK> {
+    pub fn new(mut sdk: SDK) -> Self {
+        let assets: Box<dyn StoreMap<Hash, Asset>> = sdk.alloc_or_recover_map("assets");
+
+        Self { sdk, assets }
+    }
+
+    #[genesis]
+    fn init_genesis(&mut self, payload: InitGenesisPayload) {
+        let asset = Asset {
+            id:     payload.id,
+            name:   payload.name,
+            symbol: payload.symbol,
+            supply: payload.supply,
+            issuer: payload.issuer.clone(),
+        };
+
+        self.assets.insert(asset.id.clone(), asset.clone());
+
+        let asset_balance = AssetBalance {
+            value:     payload.supply,
+            allowance: BTreeMap::new(),
+        };
+
+        self.sdk
+            .set_account_value(&asset.issuer, asset.id, asset_balance)
+    }
+
+    #[cycles(100_00)]
+    #[read]
+    fn get_asset(&self, ctx: ServiceContext, payload: GetAssetPayload) -> ServiceResponse<Asset> {
+        if let Some(asset) = self.assets.get(&payload.id) {
+            ServiceResponse::<Asset>::from_succeed(asset)
+        } else {
+            ServiceResponse::<Asset>::from_error((101, "asset id not existed"))
+        }
+    }
+
+    #[cycles(100_00)]
+    #[read]
+    fn get_balance(
+        &self,
+        ctx: ServiceContext,
+        payload: GetBalancePayload,
+    ) -> ServiceResponse<GetBalanceResponse> {
+        if !self.assets.contains(&payload.asset_id) {
+            return ServiceResponse::<GetBalanceResponse>::from_error((
+                101,
+                "asset id not existed",
+            ));
+        }
+
+        let asset_balance = self
+            .sdk
+            .get_account_value(&payload.user, &payload.asset_id)
+            .unwrap_or(AssetBalance {
+                value:     0,
+                allowance: BTreeMap::new(),
+            });
+
+        let res = GetBalanceResponse {
+            asset_id: payload.asset_id,
+            user:     payload.user,
+            balance:  asset_balance.value,
+        };
+
+        ServiceResponse::<GetBalanceResponse>::from_succeed(res)
+    }
+
+    #[cycles(100_00)]
+    #[read]
+    fn get_allowance(
+        &self,
+        ctx: ServiceContext,
+        payload: GetAllowancePayload,
+    ) -> ServiceResponse<GetAllowanceResponse> {
+        if !self.assets.contains(&payload.asset_id) {
+            return ServiceResponse::<GetAllowanceResponse>::from_error((
+                101,
+                "asset id not existed",
+            ));
+        }
+
+        let opt_asset_balance: Option<AssetBalance> = self
+            .sdk
+            .get_account_value(&payload.grantor, &payload.asset_id);
+
+        if let Some(v) = opt_asset_balance {
+            let allowance = v.allowance.get(&payload.grantee).unwrap_or(&0);
+
+            let res = GetAllowanceResponse {
+                asset_id: payload.asset_id,
+                grantor:  payload.grantor,
+                grantee:  payload.grantee,
+                value:    *allowance,
+            };
+            ServiceResponse::<GetAllowanceResponse>::from_succeed(res)
+        } else {
+            let res = GetAllowanceResponse {
+                asset_id: payload.asset_id,
+                grantor:  payload.grantor,
+                grantee:  payload.grantee,
+                value:    0,
+            };
+            ServiceResponse::<GetAllowanceResponse>::from_succeed(res)
+        }
+    }
+
+    #[cycles(210_00)]
+    #[write]
+    fn create_asset(
+        &mut self,
+        ctx: ServiceContext,
+        payload: CreateAssetPayload,
+    ) -> ServiceResponse<Asset> {
+        let caller = ctx.get_caller();
+        let payload_res = serde_json::to_string(&payload);
+
+        if let Err(e) = payload_res {
+            return ServiceResponse::<Asset>::from_error((103, format!("{:?}", e).as_str()));
+        }
+        let payload_str = payload_res.unwrap();
+
+        let id = Hash::digest(Bytes::from(payload_str + &caller.as_hex()));
+
+        if self.assets.contains(&id) {
+            return ServiceResponse::<Asset>::from_error((102, "asset id existed"));
+        }
+        let asset = Asset {
+            id:     id.clone(),
+            name:   payload.name,
+            symbol: payload.symbol,
+            supply: payload.supply,
+            issuer: caller,
+        };
+        self.assets.insert(id, asset.clone());
+
+        let asset_balance = AssetBalance {
+            value:     payload.supply,
+            allowance: BTreeMap::new(),
+        };
+
+        self.sdk
+            .set_account_value(&asset.issuer, asset.id.clone(), asset_balance);
+
+        emit_event!(ctx, asset);
+
+        ServiceResponse::<Asset>::from_succeed(asset)
+    }
+
+    #[cycles(210_00)]
+    #[write]
+    fn transfer(&mut self, ctx: ServiceContext, payload: TransferPayload) -> ServiceResponse<()> {
+        let caller = ctx.get_caller();
+        let asset_id = payload.asset_id.clone();
+        let value = payload.value;
+        let to = payload.to;
+
+        if !self.assets.contains(&payload.asset_id) {
+            return ServiceResponse::<()>::from_error((101, "asset id not existed"));
+        }
+
+        if let Err(e) = self._transfer(caller.clone(), to.clone(), asset_id.clone(), value) {
+            return ServiceResponse::<()>::from_error((106, format!("{:?}", e).as_str()));
+        };
+
+        let event = TransferEvent {
+            asset_id,
+            from: caller,
+            to,
+            value,
+        };
+
+        emit_event!(ctx, event);
+
+        ServiceResponse::<()>::from_succeed(())
+    }
+
+    #[cycles(210_00)]
+    #[write]
+    fn approve(&mut self, ctx: ServiceContext, payload: ApprovePayload) -> ServiceResponse<()> {
+        let caller = ctx.get_caller();
+        let asset_id = payload.asset_id.clone();
+        let value = payload.value;
+        let to = payload.to;
+
+        if caller == to {
+            return ServiceResponse::<()>::from_error((104, "cann't approve to yourself"));
+        }
+
+        if !self.assets.contains(&payload.asset_id) {
+            return ServiceResponse::<()>::from_error((101, "asset id not existed"));
+        }
+
+        let mut caller_asset_balance: AssetBalance = self
+            .sdk
+            .get_account_value(&caller, &asset_id)
+            .unwrap_or(AssetBalance {
+                value:     0,
+                allowance: BTreeMap::new(),
+            });
+        caller_asset_balance
+            .allowance
+            .entry(to.clone())
+            .and_modify(|e| *e = value)
+            .or_insert(value);
+
+        self.sdk
+            .set_account_value(&caller, asset_id.clone(), caller_asset_balance);
+
+        let event = ApproveEvent {
+            asset_id,
+            grantor: caller,
+            grantee: to,
+            value,
+        };
+
+        emit_event!(ctx, event);
+
+        ServiceResponse::<()>::from_succeed(())
+    }
+
+    #[cycles(210_00)]
+    #[write]
+    fn transfer_from(
+        &mut self,
+        ctx: ServiceContext,
+        payload: TransferFromPayload,
+    ) -> ServiceResponse<()> {
+        let caller = ctx.get_caller();
+        let sender = payload.sender;
+        let recipient = payload.recipient;
+        let asset_id = payload.asset_id;
+        let value = payload.value;
+
+        if !self.assets.contains(&asset_id) {
+            return ServiceResponse::<()>::from_error((101, "asset id not existed"));
+        }
+
+        let mut sender_asset_balance: AssetBalance = self
+            .sdk
+            .get_account_value(&sender, &asset_id)
+            .unwrap_or(AssetBalance {
+                value:     0,
+                allowance: BTreeMap::new(),
+            });
+        let sender_allowance = sender_asset_balance
+            .allowance
+            .entry(caller.clone())
+            .or_insert(0);
+        if *sender_allowance < value {
+            return ServiceResponse::<()>::from_error((105, "insufficient balance"));
+        }
+        let after_sender_allowance = *sender_allowance - value;
+        sender_asset_balance
+            .allowance
+            .entry(caller.clone())
+            .and_modify(|e| *e = after_sender_allowance)
+            .or_insert(after_sender_allowance);
+        self.sdk
+            .set_account_value(&sender, asset_id.clone(), sender_asset_balance);
+
+        if let Err(e) = self._transfer(sender.clone(), recipient.clone(), asset_id.clone(), value) {
+            return ServiceResponse::<()>::from_error((106, format!("{:?}", e).as_str()));
+        };
+
+        let event = TransferFromEvent {
+            asset_id,
+            caller,
+            sender,
+            recipient,
+            value,
+        };
+
+        emit_event!(ctx, event);
+
+        ServiceResponse::<()>::from_succeed(())
+    }
+
+    fn _transfer(
+        &mut self,
+        sender: Address,
+        recipient: Address,
+        asset_id: Hash,
+        value: u64,
+    ) -> Result<(), String> {
+        if recipient == sender {
+            return Err("cann't send value to yourself".to_owned());
+        }
+
+        let mut sender_asset_balance: AssetBalance = self
+            .sdk
+            .get_account_value(&sender, &asset_id)
+            .unwrap_or(AssetBalance {
+                value:     0,
+                allowance: BTreeMap::new(),
+            });
+        let sender_balance = sender_asset_balance.value;
+
+        if sender_balance < value {
+            return Err("insufficient balance".to_owned());
+        }
+
+        let mut to_asset_balance: AssetBalance = self
+            .sdk
+            .get_account_value(&recipient, &asset_id)
+            .unwrap_or(AssetBalance {
+                value:     0,
+                allowance: BTreeMap::new(),
+            });
+
+        let (v, overflow) = to_asset_balance.value.overflowing_add(value);
+        if overflow {
+            return Err("u64 overflow".to_owned());
+        }
+        to_asset_balance.value = v;
+
+        self.sdk
+            .set_account_value(&recipient, asset_id.clone(), to_asset_balance);
+
+        let (v, overflow) = sender_balance.overflowing_sub(value);
+        if overflow {
+            return Err("u64 overflow".to_owned());
+        }
+        sender_asset_balance.value = v;
+        self.sdk
+            .set_account_value(&sender, asset_id, sender_asset_balance);
+
+        Ok(())
+    }
+}
---
+++ built-in-services/asset/src/types.rs
@@ -0,0 +1,179 @@
+use std::collections::BTreeMap;
+
+use muta_codec_derive::RlpFixedCodec;
+use serde::{Deserialize, Serialize};
+
+use binding_macro::{SchemaEvent, SchemaObject};
+use protocol::fixed_codec::{FixedCodec, FixedCodecError};
+use protocol::traits::MetaGenerator;
+use protocol::types::{Address, Bytes, DataMeta, FieldMeta, Hash, StructMeta};
+use protocol::ProtocolResult;
+
+/// Payload
+#[derive(RlpFixedCodec, Deserialize, Serialize, Clone, Debug)]
+pub struct InitGenesisPayload {
+    pub id:     Hash,
+    pub name:   String,
+    pub symbol: String,
+    pub supply: u64,
+    pub issuer: Address,
+}
+
+#[derive(RlpFixedCodec, Deserialize, Serialize, Clone, Debug, SchemaObject)]
+pub struct CreateAssetPayload {
+    pub name:   String,
+    pub symbol: String,
+    pub supply: u64,
+}
+
+#[derive(RlpFixedCodec, Deserialize, Serialize, Clone, Debug, SchemaObject)]
+pub struct GetAssetPayload {
+    #[description("Asset id")]
+    pub id: Hash,
+}
+
+#[derive(RlpFixedCodec, Deserialize, Serialize, Clone, Debug, SchemaObject)]
+pub struct TransferPayload {
+    pub asset_id: Hash,
+    pub to:       Address,
+    pub value:    u64,
+}
+
+#[derive(RlpFixedCodec, Deserialize, Serialize, Clone, Debug, SchemaObject)]
+pub struct TransferEvent {
+    pub asset_id: Hash,
+    pub from:     Address,
+    pub to:       Address,
+    pub value:    u64,
+}
+
+#[derive(RlpFixedCodec, Deserialize, Serialize, Clone, Debug, SchemaObject)]
+pub struct ApprovePayload {
+    pub asset_id: Hash,
+    pub to:       Address,
+    pub value:    u64,
+}
+
+#[derive(RlpFixedCodec, Deserialize, Serialize, Clone, Debug, SchemaObject)]
+pub struct ApproveEvent {
+    pub asset_id: Hash,
+    pub grantor:  Address,
+    pub grantee:  Address,
+    pub value:    u64,
+}
+
+#[derive(RlpFixedCodec, Deserialize, Serialize, Clone, Debug, SchemaObject)]
+pub struct TransferFromPayload {
+    pub asset_id:  Hash,
+    pub sender:    Address,
+    pub recipient: Address,
+    pub value:     u64,
+}
+
+#[derive(RlpFixedCodec, Deserialize, Serialize, Clone, Debug, SchemaObject)]
+pub struct TransferFromEvent {
+    pub asset_id:  Hash,
+    pub caller:    Address,
+    pub sender:    Address,
+    pub recipient: Address,
+    pub value:     u64,
+}
+
+#[derive(RlpFixedCodec, Deserialize, Serialize, Clone, Debug, SchemaObject)]
+pub struct GetBalancePayload {
+    pub asset_id: Hash,
+    pub user:     Address,
+}
+
+#[derive(RlpFixedCodec, Deserialize, Serialize, Clone, Debug, Default, SchemaObject)]
+pub struct GetBalanceResponse {
+    pub asset_id: Hash,
+    pub user:     Address,
+    pub balance:  u64,
+}
+
+#[derive(RlpFixedCodec, Deserialize, Serialize, Clone, Debug, SchemaObject)]
+pub struct GetAllowancePayload {
+    pub asset_id: Hash,
+    pub grantor:  Address,
+    pub grantee:  Address,
+}
+
+#[derive(RlpFixedCodec, Deserialize, Serialize, Clone, Debug, Default, SchemaObject)]
+pub struct GetAllowanceResponse {
+    pub asset_id: Hash,
+    pub grantor:  Address,
+    pub grantee:  Address,
+    pub value:    u64,
+}
+
+#[derive(RlpFixedCodec, Deserialize, Serialize, Clone, Debug, PartialEq, Default, SchemaObject)]
+pub struct Asset {
+    pub id:     Hash,
+    pub name:   String,
+    pub symbol: String,
+    pub supply: u64,
+    pub issuer: Address,
+}
+
+pub struct AssetBalance {
+    pub value:     u64,
+    pub allowance: BTreeMap<Address, u64>,
+}
+
+#[derive(RlpFixedCodec)]
+struct AllowanceCodec {
+    pub addr:  Address,
+    pub total: u64,
+}
+
+#[derive(SchemaEvent)]
+pub enum Event {
+    Asset,
+    TransferEvent,
+    ApproveEvent,
+    TransferFromEvent,
+}
+
+impl rlp::Decodable for AssetBalance {
+    fn decode(rlp: &rlp::Rlp) -> Result<Self, rlp::DecoderError> {
+        let value = rlp.at(0)?.as_val()?;
+        let codec_list: Vec<AllowanceCodec> = rlp::decode_list(rlp.at(1)?.as_raw());
+        let mut allowance = BTreeMap::new();
+        for v in codec_list {
+            allowance.insert(v.addr, v.total);
+        }
+
+        Ok(AssetBalance { value, allowance })
+    }
+}
+
+impl rlp::Encodable for AssetBalance {
+    fn rlp_append(&self, s: &mut rlp::RlpStream) {
+        s.begin_list(2);
+        s.append(&self.value);
+
+        let mut codec_list = Vec::with_capacity(self.allowance.len());
+
+        for (address, allowance) in self.allowance.iter() {
+            let fixed_codec = AllowanceCodec {
+                addr:  address.clone(),
+                total: *allowance,
+            };
+
+            codec_list.push(fixed_codec);
+        }
+
+        s.append_list(&codec_list);
+    }
+}
+
+impl FixedCodec for AssetBalance {
+    fn encode_fixed(&self) -> ProtocolResult<Bytes> {
+        Ok(Bytes::from(rlp::encode(self)))
+    }
+
+    fn decode_fixed(bytes: Bytes) -> ProtocolResult<Self> {
+        Ok(rlp::decode(bytes.as_ref()).map_err(FixedCodecError::from)?)
+    }
+}
